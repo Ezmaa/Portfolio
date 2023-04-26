12edda42d03d6513a51e597662c26f49749c54e0
ezmaa <ezinmark@gmail.com>
2023-04-26 18:01:10 -0400

    added smooth scroll

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useState } from "react";
-
+import { Link } from "react-scroll";
 
 const NavBar = () => {
 
@@ -49,12 +49,19 @@ const NavBar = () => {
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
           {links.map(({ id, link }) => (
-            <li
-              key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl"
-            >
-                {link}
-            </li>
+           <li
+           key={id}
+           className="px-4 cursor-pointer capitalize py-6 text-4xl"
+         >
+           <Link
+             onClick={() => setNav(!nav)}
+             to={link}
+             smooth
+             duration={500}
+           > 
+             {link}
+             </Link>
+         </li>
           ))}
         </ul>
       )}
