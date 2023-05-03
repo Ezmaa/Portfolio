1680c707d149b5ec6f160bcb2c315baa6af74201
ezmaa <ezinmark@gmail.com>
2023-05-02 20:12:33 -0400

    changed background color

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -77,7 +77,7 @@ const Skills = () => {
       return (
         <div
           name="experience"
-          className="bg-gradient-to-b from-gray-800 to-gray-600 w-full h-screen"
+          className="bg-gray-600 w-full h-screen"
         >
           <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
             <div>
